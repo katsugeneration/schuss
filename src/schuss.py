41452b9f371eb0a6d4a251d9c667f986807a4729
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2018-01-03 21:01:57 +0900

    Add pickup candidate sentence function

--- src/schuss.py
+++ src/schuss.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from gevent.pool import Pool
 from util.sliding import sliding_window
 
 
@@ -6,40 +7,83 @@ class Schuss(object):
     '''
     Spell checker main module.
     '''
-    def __init__(self, counter, tokenizer, smoother, distance):
+    def __init__(self, counter, tokenizer, smoother, distance, window_size=3):
         '''
         :parameters:
             counter: n-gram counter object. Expect counted.
             tokenizer: tokenizer object. SentecePiece or mecab ot etc...
             smoother: probability smoothing object.
             distance: words distance  mesurement object.
+            window_size: considering window size before word.
         '''
         self.counter = counter
         self.tokenizer = tokenizer
         self.smoother = smoother
         self.distance = distance
+        self.window_size = window_size
 
-    def detect(self, sentence, window_size=3, correct_threshold=0.05):
+    def detect(self, sentence, correct_threshold=0.05):
         '''
         detect sentence miss position.
         :parameters:
-            window_size: considering window size before word.
             correct_threshold: probability threshold for correct word order judgement.
         '''
         words = self.tokenizer.encode(sentence)
         costs = [0] * len(words)
 
-        for i, s in enumerate(sliding_window(words, window_size)):
+        for i, s in enumerate(sliding_window(words, self.window_size)):
             p = self.smoother.smooth(self.counter, s)
             if p >= correct_threshold:
                 continue
-            for j in range(i, i + window_size):
+            for j in range(i, i + self.window_size):
                 costs[j] += -1
 
         return words, costs
 
-    def pickup(self, words, costs):
-        pass
+    def pickup(self, words, costs, num=10, distance=1, cost_threshold=-2, beta=0.5):
+        # TODO: speed up!!
+        candidates = []
+        for i, w in enumerate(words):
+            if costs[i] > cost_threshold:
+                candidates.append([(w, 0)])
+            else:
+                c = self._pickup_candidate_item(w, distance)
+                if (w, 0) not in c:
+                    c += [(w, 0)]
+                print(len(c))
+                candidates.append(c)
+
+        pool = Pool(10)
+
+        cs = [("", 0.0)]
+
+        def _calc(args):
+            c, s = args
+            sentence = s[0] + c[0]
+            rate = s[1] \
+                + (beta ** (c[1] + 1)) \
+                + self.smoother.smooth(self.counter, sentence[-self.window_size:])
+            return (sentence, rate)
+
+        for candidate in candidates:
+            before_cs = cs
+            cs = []
+
+            def items():
+                for c in candidate:
+                    for s in before_cs:
+                        yield (c, s)
+            
+            cs = pool.map(_calc, items())
+            cs = sorted(cs, key=lambda r: r[1], reverse=True)[:num]
+            print(cs)
+
+        return cs
+
+    def _pickup_candidate_item(self, word, distance):
+        return list(map(lambda w: (w, self.distance.measure(word, w)),
+                    filter(lambda w: self.distance.measure(word, w) <= distance,
+                    self.tokenizer.vocab)))
 
     def fix(self, sentence):
         pass
--- test/test_schuss.py
+++ test/test_schuss.py
@@ -40,7 +40,7 @@ class TestLossyCounting:
             lc.fit(f)
 
         schuss = Schuss(lc, tokenizer, smoother, None)
-        words, counts = schuss.detect("北にアゼルバイジャン、アルメニア、トルクメニスタン。東にパキスタン、アフガニスタン、西にトルコ、イラクと境を接する", window_size=window_size, correct_threshold=correct_threshold)
+        words, counts = schuss.detect("北にアゼルバイジャン、アルメニア、トルクメニスタン。東にパキスタン、アフガニスタン、西にトルコ、イラクと境を接する", correct_threshold=correct_threshold)
         arr = ['、', 'トルクメニスタン', '。', '東', 'に']
         for i, s in enumerate(sliding_window(arr, window_size)):
             if smoother.smooth(lc, s) < correct_threshold:
@@ -48,10 +48,52 @@ class TestLossyCounting:
                 assert_true(counts[i + 6] < 0)
                 assert_true(counts[i + 7] < 0)
 
-        words, counts = schuss.detect("北にアゼルバイジャン、アルメニア、トルクメニスタン、東にパキスタン、アフガニスタン、西にトルコ、イラクと境を接する", window_size=window_size, correct_threshold=correct_threshold)
+        words, counts = schuss.detect("北にアゼルバイジャン、アルメニア、トルクメニスタン、東にパキスタン、アフガニスタン、西にトルコ、イラクと境を接する", correct_threshold=correct_threshold)
         arr = ['、', 'トルクメニスタン', '、', '東', 'に']
         for i, s in enumerate(sliding_window(arr, window_size)):
             if smoother.smooth(lc, s) < 0.01:
                 assert_true(counts[i + 5] < 0)
                 assert_true(counts[i + 6] < 0)
                 assert_true(counts[i + 7] < 0)
+
+    def test_pickup_item(self):
+        from counter.lossy_counting import LossyCountingNGram
+        from tokenizer.sp_tokenizer import SentencePieceTokenizer
+        from smoother.laplace_smoother import LaplaceSmoother
+        from distance.levenshtein import Levenshtein
+        from schuss import Schuss
+
+        lc = LossyCountingNGram(window_size=3, epsilon=1e-5)
+        tokenizer = SentencePieceTokenizer('data/test.model', 'data/test.vocab')
+        smoother = LaplaceSmoother(delta=1)
+        l = Levenshtein()
+
+        schuss = Schuss(lc, tokenizer, smoother, l)
+        assert_equal([('トルクメニスタン', 1)], schuss._pickup_candidate_item("トルクメニステン", 2))
+        assert_true([] != schuss._pickup_candidate_item("であろ", 2))
+        assert_equal([], schuss._pickup_candidate_item("ああああ", 2))
+        assert_equal([], schuss._pickup_candidate_item("トルクメニステ", 1))
+        assert_true([] != schuss._pickup_candidate_item("ああああ", 3))
+
+    @parameterized([
+        (3, 1e-4, 0.01)
+    ])
+    def test_pickup(self, window_size, epsilon, correct_threshold):
+        from counter.lossy_counting import LossyCountingNGram
+        from tokenizer.sp_tokenizer import SentencePieceTokenizer
+        from smoother.laplace_smoother import LaplaceSmoother
+        from distance.levenshtein import Levenshtein
+        from schuss import Schuss
+
+        lc = LossyCountingNGram(window_size=window_size, epsilon=epsilon)
+        tokenizer = SentencePieceTokenizer('data/test.model', 'data/test.vocab')
+        smoother = LaplaceSmoother(delta=1)
+        l = Levenshtein()
+        with open('data/sp.txt', 'r', encoding='utf-8') as f:
+            lc.fit(f)
+
+        schuss = Schuss(lc, tokenizer, smoother, l)
+        # words, counts = schuss.detect("北にアゼルバイジャン、アルメニア、トルクメニスタン。東にパキスタン、アフガニスタン、西にトルコ、イラクと境を接する", correct_threshold=correct_threshold)
+        words, counts = schuss.detect("今日は晴れますか？", correct_threshold=correct_threshold)
+        ret = schuss.pickup(words, counts)
+        print(ret)
