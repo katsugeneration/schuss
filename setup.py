2e3e2020142ccbbc2aa6179fedd3f97119be2ef9
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2018-01-21 20:22:56 +0900

    Fix setup for building so module

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup
+from distutils.extension import Extension
 from Cython.Build import cythonize
 
 setup(
-    ext_modules=cythonize("src/distance/levenshtein.pyx")
+    ext_modules=cythonize([Extension("src/distance/levenshtein", ["src/distance/levenshtein.pyx"])])
 )
